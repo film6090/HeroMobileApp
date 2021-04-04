acffc64b79eb1116ebf9502a1d7af0c198bc01e7
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-04 20:00:39 +0700

    Add heroes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+//import { ItemsComponent } from './item/items.component'
+//import { ItemDetailComponent } from './item/item-detail.component'
+
+import { HeroesComponent } from './heroes/heroes.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/items', pathMatch: 'full' },
-  { path: 'items', component: ItemsComponent },
-  { path: 'item/:id', component: ItemDetailComponent },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  //{ path: 'items', component: ItemsComponent },
+  //{ path: 'item/:id', component: ItemDetailComponent },
+  { path: 'heroes' , component:HeroesComponent }
 ]
 
 @NgModule({
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ns-heroes',
+  templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+   
+  }
+}
