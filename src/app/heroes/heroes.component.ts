0ac8eaa7a0c28b8e2382a5aba660821e71753641
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-08 12:44:53 +0700

    Bing add hero two ways binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,15 +10,20 @@ import { HeroService } from '../hero.service'
 export class HeroesComponent implements OnInit {
   title = "Tour of Heroes"
   heroes : Array<Hero>;
-
+  heroName : string;
   constructor( private heroService :HeroService ) { }
 
   ngOnInit(): void {
     this.heroes = this.heroService.getHeroes()
   }
-  add( name:string ){
-    if(name!=''){
-      this.heroService.getnewHero( name );
+  add(){
+    console.log("active!");
+    console.log(this.heroName);
+    
+    if(this.heroName != undefined || this.heroName != null){
+      console.log("work!");
+      this.heroService.getnewHero( this.heroName );
+      this.heroName = '';
     }
   }
 }
